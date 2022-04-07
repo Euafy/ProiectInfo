a03a847c8c201ffabf3568d55d7c1dd1e798e66f
Euafy <ioncosmin682@gmail.com>
2022-04-08 01:24:44 +0300

    first page

---
+++ script.js
@@ -0,0 +1,20 @@
+function valori() {
+    document.getElementById("valori1").style.filter = "blur(0px)";
+    document.getElementById("valori1").style.border ="7px solid rgb(8, 218, 8)";
+    document.getElementById("valori2").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+}
+
+function parametri() {
+    document.getElementById("parametri1").style.filter = "blur(0px)";
+    document.getElementById("parametri1").style.border ="7px solid rgb(8, 218, 8)";
+    document.getElementById("parametri2").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+}
+
+function tablouri() {
+    document.getElementById("tablouri1").style.filter = "blur(0px)";
+    document.getElementById("tablouri1").style.border ="7px solid rgb(8, 218, 8)";
+    document.getElementById("tablouri2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+}
